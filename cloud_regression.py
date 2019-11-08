a81b8fde158c1fac6aa84106d972d37f27644da1
Charles <masarain@stanford.edu>
2019-11-07 21:20:11 -0800

    Feature experimentation

--- cloud_regression.py
+++ cloud_regression.py
@@ -1,12 +1,110 @@
 import numpy as np
 import math
 import util
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import getRGBratios
+
+
+def plot_points(x, y):
+    """Plot some points where x are the coordinates and y is the label"""
+    x_one = x[y == 0, :]
+    x_two = x[y == 1, :]
+    x_three = x[y == -1, :]
+
+    plt.scatter(x_one[:,0], x_one[:,1], marker='x', color='red')
+    plt.scatter(x_two[:,0], x_two[:,1], marker='o', color='blue')
+    plt.scatter(x_three[:, 0], x_three[:, 1], marker='x', color='green')
+
+    plt.show()
+
+
+def plot_3d(x, y):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x_one = x[y == 0, :]
+    x_two = x[y == 1, :]
+
+    ax.scatter(x_one[:, 0], x_one[:, 1], x_one[:, 2], c='red')
+    ax.scatter(x_two[:, 0], x_two[:, 1], x_two[:, 2], c='blue')
+
+    plt.show()
+
+
+
+
+
+def calc_grad(X, Y, theta):
+    """Compute the gradient of the loss with respect to theta."""
+    count, _ = X.shape
+
+    probs = 1. / (1 + np.exp(-X.dot(theta)))
+    grad = (Y - probs).dot(X)
+
+    return grad
+
+
+def logistic_regression(X, Y, theta=None):
+    """Train a logistic regression model."""
+    if theta is None:
+        theta = np.zeros(X.shape[1])
+    learning_rate = 0.1
+    # theta = np.array([-3000, 0,0])
+
+    i = 0
+    while True:
+        i += 1
+        prev_theta = theta
+        grad = calc_grad(X, Y, theta)
+        theta = theta + learning_rate * grad
+        if i % 10000 == 0:
+            print('Finished %d iterations' % i)
+        if np.linalg.norm(prev_theta - theta) < 1e-15:
+            print('Converged in %d iterations' % i)
+            print('Norm is: ' + str(np.linalg.norm(prev_theta - theta)))
+            print("Theta is: " + str(theta))
+            break
+        else:
+            if i % 10000 == 0:
+                print("Current norm is: " + str(np.linalg.norm(prev_theta - theta)))
+                print("Current theta is: " + str(theta))
+    return theta
 
 
 def main(train_path, label_path):
     images, label_dict = util.load_data_and_label(train_path, label_path)
-    print(images["train_1"])
-    print(label_dict["train_1"])
+    # print(images["train_1"])
+    # print(label_dict["train_1"])
+
+    x = []
+    y = []
+    for img_name in images.keys():
+        if "partly_cloudy" in label_dict[img_name]:
+            continue
+        # if "haze" in label_dict[img_name]:
+        #     continue
+        temp = getRGBratios.getRGBRatio(images[img_name])
+        if "haze" in label_dict[img_name] or "cloudy" in label_dict[img_name]:
+            y += [1]
+            if temp[0] < 0.1:
+                print(img_name + " with luminance value: " + str(temp[0]))
+        elif "primary" in label_dict[img_name]:
+            y += [-1]
+        else:
+            y += [0]
+
+
+        x += [temp]
+
+    print(len(y))
+    # print(np.array(x))
+    # print(y)
+
+    # x = np.log(x)
+
+    plot_points(np.array(x), np.array(y))
+    # logistic_regression(np.array(x), y)
 
 
 main("train_subset", "train_v2.csv")
--- getRGBratios.py
+++ getRGBratios.py
@@ -4,14 +4,31 @@ import numpy as np
 import util
 
 
-def getRGBRatio(filename):
-      # img = cv2.imread(filename) # BGR
-      img = util.read_jpeg(filename)
-      rows,cols, channels = img.shape
-      R = img[:,:,0]
-      G = img[:,:,1]
-      B = img[:,:,2]
-      meanR = np.mean(R[:,:])
-      meanG = np.mean(G[:,:])
-      meanB = np.mean(B[:,:])
-      return meanG/meanR, meanG/meanB
+def getRGBRatio(img):
+    # img = cv2.imread(filename) # BGR
+    # img = util.read_jpeg(filename)
+    # Assume img is RGB
+    rows,cols, channels = img.shape
+    R = img[:,:,0]
+    G = img[:,:,1]
+    B = img[:,:,2]
+
+    R2 = np.power(R, 1 * np.ones(R.shape))
+    G2 = np.power(G, 1 * np.ones(G.shape))
+    B2 = np.power(B, 1 * np.ones(B.shape))
+
+
+    luminance = 0.299 * R2 + 0.587 * G2 + 0.114 * B2
+    luminance = luminance / (3 * 255)
+
+    # R = np.power(R, 2.2 * np.ones(R.shape))
+    # G = np.power(G, 2.2 * np.ones(G.shape))
+    # B = np.power(B, 2.2 * np.ones(B.shape))
+
+    meanR = np.mean(R[:,:])
+    meanG = np.mean(G[:,:])
+    meanB = np.mean(B[:,:])
+    meanLuminance = np.mean(luminance)
+
+    return meanLuminance, meanG/meanR, meanG/meanB
+    # return meanLuminance, meanG
